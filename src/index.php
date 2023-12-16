4b6b2ab63b5360426db5fa0237f5462f09583617
nochujanggg <130625585+nochujanggg@users.noreply.github.com>
2023-12-16 18:15:00 +0800

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,5 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Git & Github Workshop</title>
+</head>
+<body>
+
+<p>My first paragraph.</p>
+
 <?php
 
 	echo "<h1>Alhamdulillah</h1>";
 	
 ?>
+
+</body>
+</html>
